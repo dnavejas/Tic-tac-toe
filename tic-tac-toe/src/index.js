b3dd94bce743b5f3272f40e858aeec8bc636ba41
Danny Navejas <dannynavejas@gmail.com>
2019-07-02 17:36:42 -0500

    refactored code to have the board handle the state of each square based on event onClick

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,8 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares });
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
   render() {
     const status = "Next player: X";
@@ -31,15 +47,9 @@ class Board extends React.Component {
   }
 }
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.state.value}
       </button>
     );
